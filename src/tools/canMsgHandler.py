9ddefcabb74147a9cbd9c20077b5bc52490adee1
JChrysanthemum <624099049@qq.com>
2020-01-03 11:56:54 +0800

    update extFrame

--- src/tools/canMsgHandler.py
+++ src/tools/canMsgHandler.py
@@ -2,19 +2,49 @@ import time
 import can
 from enum import Enum
 
+def initBuses(cfgs):
+    """Initialize CAN-buses in software
+    
+    Initialize the cab-buses first then you can use buses 
+    to send or recieve messages
+    
+    :type cfgs: Array with cuple like (channel,bitrate)
+    :param cfgs: List of CAN-BUS configure
+    
+    :return: buses
+
+    :raise can.CanError
+        if the initialization failed
+
+    .. note::
+
+        This method will just finish the set-up in software that assume you
+        have complete your hardware installation
+
+    """
+    busses = []
+
+    for cfg in cfgs:
+        try:
+            busses.append( can.interface.Bus(bustype='socketcan', channel=cfg[0], bitrate=cfg[1]))
+        except can.CanError as e:
+            print("CAN bus init failed in %s , "%cfg , e)
+
+    return busses
+
 
 class canFrame(object):
     
     class _extID_table(Enum):
-        BIT27 = 3
-        TARGET_ID_0 = 4
-        TARGET_ID_1 = 13
-        BIT18 = 13
-        SOURCE_ID_0 = 14
-        SOURCE_ID_1 = 23
-        C1 = 23
-        c2 = 24 
-        CMD0REG_0 = 25
+        BIT27 = 0
+        TARGET_ID_0 = 1
+        TARGET_ID_1 = 10
+        BIT18 = 10
+        SOURCE_ID_0 = 11
+        SOURCE_ID_1 = 20
+        C1 = 20
+        c2 = 21
+        CMD0REG_0 = 22
 
     def __init__(self):
          
@@ -52,46 +82,18 @@ class canFrame(object):
         
         #: msg.data is (byterarray)
         #: bytearray -> bytes -> string ;  
-        bytes_value=bytes(msg.data).decode(encoding="utf-8")
+        self._extID=str(bin(msg.arbitration_id))[2:].rjust(29,"0")
+               
+        
+        print(self._extID)
     
-        for asc in bytes_value:
-            value.append(bin(ord(asc)))
+        # Data frame Part
+        #bytes_value=bytes(msg.arbitration_id).decode(encoding="utf-8")
+        #for asc in bytes_value:
+        #    value.append(str(bin(ord(asc)))[:].rjust(4,"0"))
 
         self._extID = value
-        value = str(value)
-        print(value)
-
-def initBuses(cfgs):
-    """Initialize CAN-buses in software
-    
-    Initialize the cab-buses first then you can use buses 
-    to send or recieve messages
-    
-    :type cfgs: Array with cuple like (channel,bitrate)
-    :param cfgs: List of CAN-BUS configure
-    
-    :return: buses
-
-    :raise can.CanError
-        if the initialization failed
-
-    .. note::
-
-        This method will just finish the set-up in software that assume you
-        have complete your hardware installation
-
-    """
-    busses = []
-
-    for cfg in cfgs:
-        try:
-            busses.append( can.interface.Bus(bustype='socketcan', channel=cfg[0], bitrate=cfg[1]))
-        except can.CanError as e:
-            print("CAN bus init failed in %s , "%cfg , e)
-
-    return busses
-
-
+        #print(value,value[0:5])
 
 
 
@@ -103,6 +105,7 @@ if __name__=="__main__":
     while True:
         for msg in bus:
             #print(dir(msg))
-            #print(msg.dlc)
-            print(can.decode_msg(msg))
+            #print(msg.arbitration_id)
+            #print(msg.channel)
+            can.decode_msg(msg)
             #print(type(msg.data))
