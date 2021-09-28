0b94678e9de60b83df40c9afa116c47aa8f0fcfe
jxt <jiangxt404@qq.com>
2021-09-28 17:05:52 +0800

    add stat and img collection

--- src/tools/navigator.py
+++ src/tools/navigator.py
@@ -14,10 +14,15 @@ import argparse
 import os
 import sys
 import time
+import pickle
+import cv2
 from multiprocessing import Manager, Process, Value
-
+from detector import *
 from canManager import *
-from cobot import *
+from datetime import datetime as dt
+
+
+# from cobot import *
 
 # Speed of motor for plus
 # Unit : plus pre second
@@ -43,15 +48,47 @@ class Navigator(object):
         # bitrate = 500000
         self.bus.send(KinggoCAN.enable(DeviceID.motorBroadcastid))
         self.send(KinggoCAN.disable(DeviceID.motorBroadcastid))
-        self.mycobot = RobotArm()
+        # self.mycobot = RobotArm()
 
         time.sleep(0.5)
         # zeroing
         # WheelWard X Y Z
-        self.pos = [0, 0, 0, 0]
+        # self.stat = {}
+        self.readStat()
 
-        self.robot_arm_record = None
-        pass
+    def saveStat(self):
+        _f = open('robot.stat', 'w')
+        pickle.dump(self.stat, _f)
+        _f.close()
+
+    def readStat(self):
+        if os.path.exists('robot.stat'):
+            _f = open('robot.stat', 'r')
+            self.stat = pickle.load(_f)
+            _f.close()
+        else:
+            print('no robot.stat file found !') 
+            self.stat = {
+                "next_dir": 1,
+            }
+
+    def data_collection():
+        # distance_f = 10000  # 10 m
+        data_dir = os.path.join(
+            "/home/pi/data",  dt.now().strftime("%y-%n-%d"))
+        if not os.path.exists():
+            os.mkdir(data_dir)
+        step_f = 100
+        step_times = 10
+        step_dir = self.stat["next_dir"]
+        for i in range(step_times):
+            img = camera_capture()
+            img_path = os.path.join(
+                "/home/pi/data",  "%03d.png" % i)
+            cv2.imwrite(img_path, img)
+            nav.move(GroupID.groupWheel, step_f * step_dir)
+        self.stat["next_dir"] = step_dir*-1
+        self.saveStat()
 
     def send(self, msg, st=0.1):
         self.bus.send(msg)
@@ -92,35 +129,35 @@ class Navigator(object):
         print("Waiting for moving: %.2f" % time_slot)
         time.sleep(time_slot)
 
-    def show_locate(self):
+    # def show_locate(self):
 
-        # step1 Move Z to top level
-        self.move(DeviceID.motorSlideZ1, 500)
+    #     # step1 Move Z to top level
+    #     self.move(DeviceID.motorSlideZ1, 500)
 
-        # step2 rotate camera and locate the ball
-        args_list = [None, ["h", 15], ["h", -30], ["V", 15], ["h", -30]]
-        for arg in args_list:
-            if arg:
-                print("Camera rotate", arg)
-                self.mycobot.cam_rotate(arg[0], arg[1])
-            movement = self.mycobot.locate(
-                precision=0.05, color_ranges=GREEN_RANGES, min_r=0.02)
-            print("Located:\nFront: %.2f cm, Right: %.2fcm, Down: %.2fcm" %
-                  (movement[0], movement[1], movement[2]))
-            if movement:
-                delta_x = int(movement[0]*10)
-                delta_y = int(movement[1]*10)
-                delta_z = int(movement[2]*10)
-                for d, n in [[GroupID.groupWheel, delta_x], [DeviceID.motorSlideY1, delta_y],
-                             [DeviceID.motorSlideZ1, delta_z]]:
-                    if n > 40:
-                        print("Robot move %s %dmm:" % (d, n))
-                        self.move(d, n)
-                # step3 state to observer and locate again
-                self.mycobot.state(AngleAnimation.Observer)
-                self.mycobot.locate(
-                    precision=0.05, color_ranges=GREEN_RANGES, min_r=0.02)
-                break
+    #     # step2 rotate camera and locate the ball
+    #     args_list = [None, ["h", 15], ["h", -30], ["V", 15], ["h", -30]]
+    #     for arg in args_list:
+    #         if arg:
+    #             print("Camera rotate", arg)
+    #             self.mycobot.cam_rotate(arg[0], arg[1])
+    #         movement = self.mycobot.locate(
+    #             precision=0.05, color_ranges=GREEN_RANGES, min_r=0.02)
+    #         print("Located:\nFront: %.2f cm, Right: %.2fcm, Down: %.2fcm" %
+    #               (movement[0], movement[1], movement[2]))
+    #         if movement:
+    #             delta_x = int(movement[0]*10)
+    #             delta_y = int(movement[1]*10)
+    #             delta_z = int(movement[2]*10)
+    #             for d, n in [[GroupID.groupWheel, delta_x], [DeviceID.motorSlideY1, delta_y],
+    #                          [DeviceID.motorSlideZ1, delta_z]]:
+    #                 if n > 40:
+    #                     print("Robot move %s %dmm:" % (d, n))
+    #                     self.move(d, n)
+    #             # step3 state to observer and locate again
+    #             self.mycobot.state(AngleAnimation.Observer)
+    #             self.mycobot.locate(
+    #                 precision=0.05, color_ranges=GREEN_RANGES, min_r=0.02)
+    #             break
 
 
 def main():
@@ -139,9 +176,9 @@ def main():
         nav.move(DeviceID.motorSlideY1, args.n)
     elif args.act == "Z":
         nav.move(DeviceID.motorSlideZ1, args.n)
-    elif args.act == "A":
-        # animation()
-        pass
+    # elif args.act == "A":
+    #     # animation()
+    #     pass
 
 
 if __name__ == "__main__":
